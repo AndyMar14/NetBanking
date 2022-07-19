bcbdf7d0c6f94240c07acb59fd6b6c2e355eddab
AndyRafael <andyrafaelmar@gmail.com>
2022-07-18 21:30:38 -0400

    Modificacion de campos en Users

--- NetBanking.Core.Domain/Entities/Users.cs
+++ NetBanking.Core.Domain/Entities/Users.cs
@@ -9,8 +9,10 @@ namespace NetBanking.Core.Domain.Entities
     public class Users
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public int UserType { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Identification { get; set; }
         public string Email { get; set; }
         public string User { get; set; }
         public string Password { get; set; }
