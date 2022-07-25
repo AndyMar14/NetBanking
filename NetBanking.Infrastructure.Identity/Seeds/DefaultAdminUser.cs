eef3eb3e3c5f04b1faa38703f54719800146e0f1
oscarballard <60292160+oscarballard@users.noreply.github.com>
2022-07-25 18:56:21 -0500

    update

--- NetBanking.Infrastructure.Identity/Seeds/DefaultAdminUser.cs
+++ NetBanking.Infrastructure.Identity/Seeds/DefaultAdminUser.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-namespace StockApp.Infrastructure.Identity.Seeds
+namespace NetBanking.Infrastructure.Identity.Seeds
 {
-    public static class DefaultSuperAdminUser
+    public static class DefaultAdminUser
     {
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
