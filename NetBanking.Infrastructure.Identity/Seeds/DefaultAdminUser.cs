16598091aefe08d02e047c7c6c401581f51af675
oscarballard <60292160+oscarballard@users.noreply.github.com>
2022-07-25 18:32:05 -0500

    change-users

--- NetBanking.Infrastructure.Identity/Seeds/DefaultAdminUser.cs
+++ NetBanking.Infrastructure.Identity/Seeds/DefaultAdminUser.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using NetBanking.Core.Application.Enums;
 using NetBanking.Infrastructure.Identity.Entities;
+using System.Linq;
 using System.Threading.Tasks;
 
-namespace NetBanking.Infrastructure.Identity.Seeds
+
+namespace StockApp.Infrastructure.Identity.Seeds
 {
-    public static class DefaultAdminUser
+    public static class DefaultSuperAdminUser
     {
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Client.ToString()));
+            ApplicationUser defaultUser = new();
+            defaultUser.UserName = "superadminuser";
+            defaultUser.Email = "superadminuser@email.com";
+            defaultUser.FirstName = "John";
+            defaultUser.LastName = "Doe";
+            defaultUser.EmailConfirmed = true;
+            defaultUser.PhoneNumberConfirmed = true;
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word!");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Client.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                }
+            }
+
         }
     }
 }
